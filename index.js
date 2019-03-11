4b1179c99b4ce1d30d04cc05eaaacb1feed6aa46
Nick Inman <ninman@Nicks-MBP.fios-router.home>
2019-03-11 11:59:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  } else if (someValue > 2000 && someValue < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(someValue) {
+  return someValue === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+}
+
+function switchOnCharmFromTip(someValue) {
+  switch (someValue) {
+    case 'generous':
+      return 'Thank you so much.'
+      break
+    case 'not as generous':
+      return 'Thank you.'
+      break
+    default:
+      return 'Bye.'
+      break
+  }
+}
+
+// scuberGreetingForFeet() — Use if and else if statements to return the correct greeting based on the distance the passenger desires to travel.
+// ternaryCheckCity() — Use a ternary operator to return the correct response based on the desired destination of the passenger.
+// switchOnCharmFromTip() — Use a switch statement to return a different response based on the generosity of the passenger's tip.
